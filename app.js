721f7a39b380241cd89507e0fa04cf02f323ae1d
ezeguaimas <ezequiel.guaimas@gmail.com>
2023-05-15 18:33:05 -0300

    correccion de espacios y se agrega corazoncito

--- app.js
+++ app.js
@@ -13,99 +13,97 @@ function calcular() {
   let sueldoBruto = (aporte1 + aporte2 + aporte3 + aporte4) / 0.03;
   // let sueldoBruto1 = (aporte1 + aporte2) / 0.03;
   // let sueldoBruto2 = (aporte3 + aporte4) / 0.03;
-  
-    const montoMaximo = 642142.18;
 
-    if (sueldoBruto > montoMaximo) {
-      sueldoBruto = montoMaximo;
-    }
+  const montoMaximo = 642142.18;
 
-  
-    // if (sueldoBruto1 > montoMaximo) {
-    //   sueldoBruto1 = montoMaximo;
-    // }
-    // if (sueldoBruto2 > montoMaximo) {
-    //   sueldoBruto2 = montoMaximo;
-    // }
+  if (sueldoBruto > montoMaximo) {
+    sueldoBruto = montoMaximo;
+  }
 
-    // if (adultos === "") {
-    //   adultos = 0;
-    // }
-    if (menores === "") {
-      menores = 0;
-    }
+  // if (sueldoBruto1 > montoMaximo) {
+  //   sueldoBruto1 = montoMaximo;
+  // }
+  // if (sueldoBruto2 > montoMaximo) {
+  //   sueldoBruto2 = montoMaximo;
+  // }
 
-    if (adultos === "") {
-      const mensajeError = document.getElementById("mensaje-error");
-      mensajeError.textContent = "Ingrese valores para adultos y menores";
-      setTimeout(function() {
-        mensajeError.textContent = "";
-      }, 3000);
-      return;
-    }
+  // if (adultos === "") {
+  //   adultos = 0;
+  // }
+  if (menores === "") {
+    menores = 0;
+  }
 
-    // if (isNaN(adultos) || isNaN(menores)) {
-    //   const mensajeError = document.getElementById("mensaje-error");
-    //   mensajeError.textContent = "Ingrese valores para adultos y menores";
-    
-    //   // Establecer un temporizador para eliminar el mensaje de error después de 3 segundos
-    //   setTimeout(function() {
-    //     mensajeError.textContent = "";
-    //   }, 3000);
-    
-    //   return;
-    // }
+  if (adultos === "") {
+    const mensajeError = document.getElementById("mensaje-error");
+    mensajeError.textContent = "Ingrese valores para adultos y menores";
+    setTimeout(function () {
+      mensajeError.textContent = "";
+    }, 3000);
+    return;
+  }
 
-    const aporteOS = sueldoBruto * 0.0765;
-        
-    // const aporteOS1 = sueldoBruto1 * 0.0765;
-    // const aporteOS2 = sueldoBruto2 * 0.0765;
-    const aporteTotal = aporteOS
-    // const aporteTotal = aporteOS1 + aporteOS2
-    const corpDif = aporteTotal - ((corpAdulto * adultos) + (corpMenor * menores))
-    const vipDif = aporteTotal - ((vipAdulto * adultos) + (vipMenor * menores))
-    let renderCorp = 0
-    let renderVip = 0
+  // if (isNaN(adultos) || isNaN(menores)) {
+  //   const mensajeError = document.getElementById("mensaje-error");
+  //   mensajeError.textContent = "Ingrese valores para adultos y menores";
 
-    if (corpDif > 0){
-      renderCorp = "No Paga Diferencia"
-    }else{
-      renderCorp = Math.abs(corpDif)
-    }
+  //   // Establecer un temporizador para eliminar el mensaje de error después de 3 segundos
+  //   setTimeout(function() {
+  //     mensajeError.textContent = "";
+  //   }, 3000);
 
-    if (vipDif > 0){
-      renderVip = "No Paga Diferencia"
-    }else{
-      renderVip = Math.abs(vipDif)
-    }
+  //   return;
+  // }
 
-    
-  
-    // Mostramos los resultados en el HTML
-    document.getElementById("sueldo_bruto").textContent = sueldoBruto.toFixed(2);
-    // document.getElementById("sueldo_bruto_1").textContent = sueldoBruto1.toFixed(2);
-    // document.getElementById("sueldo_bruto_2").textContent = sueldoBruto2.toFixed(2);
-    document.getElementById("aporte_os").textContent = aporteOS.toFixed(2);
-    // document.getElementById("aporte_os_1").textContent = aporteOS1.toFixed(2);
-    // document.getElementById("aporte_os_2").textContent = aporteOS2.toFixed(2);
-    document.getElementById("aporte_total").textContent = aporteTotal.toFixed(2);
-    document.getElementById("dif_corp").textContent = typeof renderCorp === 'number' ? renderCorp.toFixed(2) : renderCorp;
-    document.getElementById("dif_vip").textContent = typeof renderVip === 'number' ? renderVip.toFixed(2) : renderVip;
+  const aporteOS = sueldoBruto * 0.0765;
+
+  // const aporteOS1 = sueldoBruto1 * 0.0765;
+  // const aporteOS2 = sueldoBruto2 * 0.0765;
+  const aporteTotal = aporteOS;
+  // const aporteTotal = aporteOS1 + aporteOS2
+  const corpDif = aporteTotal - (corpAdulto * adultos + corpMenor * menores);
+  const vipDif = aporteTotal - (vipAdulto * adultos + vipMenor * menores);
+  let renderCorp = 0;
+  let renderVip = 0;
+
+  if (corpDif > 0) {
+    renderCorp = "No Paga Diferencia";
+  } else {
+    renderCorp = Math.abs(corpDif);
+  }
+
+  if (vipDif > 0) {
+    renderVip = "No Paga Diferencia";
+  } else {
+    renderVip = Math.abs(vipDif);
+  }
+
+  // Mostramos los resultados en el HTML
+  document.getElementById("sueldo_bruto").textContent = sueldoBruto.toFixed(2);
+  // document.getElementById("sueldo_bruto_1").textContent = sueldoBruto1.toFixed(2);
+  // document.getElementById("sueldo_bruto_2").textContent = sueldoBruto2.toFixed(2);
+  document.getElementById("aporte_os").textContent = aporteOS.toFixed(2);
+  // document.getElementById("aporte_os_1").textContent = aporteOS1.toFixed(2);
+  // document.getElementById("aporte_os_2").textContent = aporteOS2.toFixed(2);
+  document.getElementById("aporte_total").textContent = aporteTotal.toFixed(2);
+  document.getElementById("dif_corp").textContent =
+    typeof renderCorp === "number" ? renderCorp.toFixed(2) : renderCorp;
+  document.getElementById("dif_vip").textContent =
+    typeof renderVip === "number" ? renderVip.toFixed(2) : renderVip;
 }
-  
-  const borrarBtn = document.getElementById("borrar");
-  borrarBtn.addEventListener("click", function () {
-    document.getElementById("aporte1").value = "";
-    document.getElementById("aporte2").value = "";
-    document.getElementById("aporte3").value = "";
-    document.getElementById("aporte4").value = "";
-    document.getElementById("adultos").value = "";
-    document.getElementById("menores").value = "";
-    document.getElementById("sueldo_bruto").textContent = "";
-    document.getElementById("aporte_os").textContent = "";
-    document.getElementById("aporte_total").textContent = "";
-    document.getElementById("dif_corp").textContent = "";
-    document.getElementById("dif_vip").textContent = "";
-    calcular()
-  });
-  
+
+const borrarBtn = document.getElementById("borrar");
+borrarBtn.addEventListener("click", function () {
+  document.getElementById("aporte1").value = "";
+  document.getElementById("aporte2").value = "";
+  document.getElementById("aporte3").value = "";
+  document.getElementById("aporte4").value = "";
+  document.getElementById("adultos").value = "";
+  document.getElementById("menores").value = "";
+  document.getElementById("sueldo_bruto").textContent = "";
+  document.getElementById("aporte_os").textContent = "";
+  document.getElementById("aporte_total").textContent = "";
+  document.getElementById("dif_corp").textContent = "";
+  document.getElementById("dif_vip").textContent = "";
+  calcular();
+});
